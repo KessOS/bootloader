91d7861add91b31ebb26300eb4801b1df223f41a
5purious <spruiousvector@gmail.com>
2022-06-15 21:50:16 -0400

    refactor: Cleaned up.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 #![feature(abi_efiapi)]
 
 use uefi::prelude::*;
-use uefi::CStr16 as c16;
 use core::fmt::Write;
 
 
@@ -12,8 +11,8 @@ fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
 
     let stdout = system_table.stdout();
-    stdout.clear();
-    stdout.write_str("Hello, World!");
+    stdout.clear().unwrap();
+    stdout.write_str("Hello, World!").unwrap();
 
     loop {}
 }
